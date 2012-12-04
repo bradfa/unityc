f7c7c9f6ee16de77413a675308e400a791c852d9
Andrew Bradford <andrew@bradfordembedded.com>
2012-12-04 11:13:21 -0500

    unity_fixture_malloc: Don't use unity_malloc and friends
    They cause issues on 64 bit platforms.  They are nice to have, though.
    
    Refs #8

--- include/unity_fixture_malloc_overrides.h
+++ include/unity_fixture_malloc_overrides.h
@@ -7,9 +7,12 @@
 #ifndef UNITY_FIXTURE_MALLOC_OVERRIDES_H_
 #define UNITY_FIXTURE_MALLOC_OVERRIDES_H_
 
-#define malloc  unity_malloc
-#define calloc  unity_calloc
-#define realloc unity_realloc
-#define free	unity_free
+/* FIXME: These cause issues on 64 bit platforms. */
+/*
+ *#define malloc  unity_malloc
+ *#define calloc  unity_calloc
+ *#define realloc unity_realloc
+ *#define free	unity_free
+ */
 
 #endif /* UNITY_FIXTURE_MALLOC_OVERRIDES_H_ */
